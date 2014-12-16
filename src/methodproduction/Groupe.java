bb8ee84e515f8a6d43c975fd535700624284deff
arthur <arthur@172.31.247.40>
2014-12-16 12:28:45 +0100

    euh je veux pull :P

---
+++ src/methodproduction/Groupe.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package methodproduction;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author arthur
+ */
+public class Groupe {
+    public int point;
+    public ArrayList<Equipe> groupe;
+    public ArrayList<Equipe>[][][] tab;
+
+    public Groupe(ArrayList<Equipe> groupe) {
+        this.groupe = groupe;
+    }
+    
+    public ArrayList<Equipe> rencontre(ArrayList<Equipe> groupe){
+        for(int i = 0; i<groupe.size();i++){
+          
+        }
+        
+        
+        
+        
+        return groupe;
+    }
+    
+    
+}
